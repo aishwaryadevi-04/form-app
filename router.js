08efa4aa5581ab6dda39ab58d79028a73366ccfc
aishwaryadevi-04 <105126078+aishwaryadevi-04@users.noreply.github.com>
2023-03-25 10:13:28 +0530

    Update router.js

--- router.js
+++ router.js
@@ -6,33 +6,22 @@ const User = require('./user.model')
 const mongoose = require('mongoose');
 
 
-router.post('/form',(req,res)=>{
-
-    User.findOne({email:req.body.email})
-    .then((user) => {
-        if (user) {
-            res.status(400).json({error:'User already exists'});
-        } else {
-            const newUser = new User({
-                name:req.body.name,
-                email: req.body.email,
-                password: req.body.password,
-                phoneno: req.body.phoneno
-            });
-
-            newUser.save().then(() => {
-                res.json(newUser);
-            }).catch((err) => {
-                res.json({ error: err.message });
-            });
-        }
-    }).catch((err) => {
-        res.json({ error: err.message });
+    router.post('/form',(req,res)=>{
+        res.send('Works')
+        
+        const newUser = new User({
+            name:req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            phoneno: req.body.phoneno
+        });
+    
+        newUser.save().then(() => {
+            res.json(newUser);
+        }).catch((err) => {
+            res.json({ error: err.message });
+        });
     });
-
-
-
- 
-})
+    
 
 module.exports = router
