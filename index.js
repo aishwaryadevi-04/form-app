056dac2363835c34b7957e887fc4e606a20ea91d
aishwaryadevi-04 <aishwaryadev2004@gmail.com>
2023-03-25 09:16:17 +0530

    Changes

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const app = require('./app')
 const db = require('./database')
  const userModel = require('./user.model')
 const mongoose = require('mongoose')
-
+const cors = require('cors');
 const bodyParser = require('body-parser')
 
  mongoose.connect("mongodb+srv://aishwaryadevi2004:Aish123@cluster0.szomitx.mongodb.net/form")
  app.use(bodyParser.urlencoded({extended:true}))
  app.use(bodyParser.json())
-
+ app.use(cors());
  app.use('/',require('./router'))
 
 app.get('/', (req,res)=>{
