556ef94302875350890477136482fb34dd08af1f
aishwaryadevi-04 <105126078+aishwaryadevi-04@users.noreply.github.com>
2023-06-28 20:40:08 +0530

    Add files via upload

---
+++ auth.controller.js
@@ -0,0 +1,47 @@
+const { authService } = require('../services');
+const httpStatus = require('http-status');
+const authController = {
+    async register(req,res,next){
+        try{
+            const { email, password } = req.body;
+            const user = await authService.createUser(email, password);
+            const token = await authService.genAuthToken(user);
+            
+
+            // send verification email
+
+            /// cookie
+            res.cookie('x-access-token',token)
+            .status(httpStatus.CREATED).send({
+                user,
+                token
+            })
+            
+        } catch(error){
+            next(error)
+        }
+    },
+
+
+    async signin(req,res,next){
+        try{
+            const { email, password } = req.body;
+            const user =  await authService.signInwithEmailandPassword(email,password);
+            const token = await authService.genAuthToken(user);
+            res.cookie('x-access-token',token)
+            .status(httpStatus.CREATED).send({
+                user,
+                token
+            })
+        }
+        catch(error){
+           // res.status(httpStatus.BAD_REQUEST).send(error.message)
+        next(error);
+        }
+    },
+async isauth(req,res,next){
+    res.json(req.user)
+}
+}
+
+module.exports = authController;
--- index.js
+++ index.js
@@ -1,21 +1,2 @@
-const app = require('./app')
-const db = require('./database')
- const userModel = require('./user.model')
-const mongoose = require('mongoose')
-const cors = require('cors');
-const bodyParser = require('body-parser')
-
- mongoose.connect("mongodb+srv://aishwaryadevi2004:Aish123@cluster0.szomitx.mongodb.net/form")
- app.use(bodyParser.urlencoded({extended:true}))
- app.use(bodyParser.json())
- app.use(cors());
- app.use('/',require('./router'))
-
-app.get('/', (req,res)=>{
-    res.send('Working!');
-})
-
-const port = process.env.PORT || 5000
-app.listen(port,()=>{
-    console.log("Server running on 5000")
-})
+module.exports.authService = require('./auth.service')
+module.exports.userService = require('./user.service')
